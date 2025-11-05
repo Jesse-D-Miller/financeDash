7f10d8ae47bdde181e24d163030b4443c749e9c4
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-11-05 14:15:39 -0800

    changing from bootstrap to css for better learning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,44 @@
 function NavBar() {
   return (
-    <>
-      <div className="row align-items-center"> 
-        <div className="col">
+    <nav className="navbar">
+      <div className="navbar-upper">
+        <div className="navbar-upper-start">
           <h1>Household Finance</h1>
           <p>All-in-one: spending, budgets, savings, goals, investments</p>
         </div>
-        <div className="col text-end">
-          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button className="btn btn-primary me-md-2" type="button">
-              + Add Transaction
-            </button>
-            <button className="btn btn-outline-primary" type="button">
-              🌗
-            </button>
-          </div>
+
+        <div className="navbar-upper-end">
+          <button type="button">
+            + Add Transaction
+          </button>
+          <button type="button">
+            🌗
+          </button>
         </div>
       </div>
 
-      <nav className="navbar navbar-expand bg-body-tertiary">
-        <div className="container-fluid justify-content-start">
-          {[
-            "Overview",
-            "Spending",
-            "Budget",
-            "Goals",
-            "Savings",
-            "Investments",
-            "Net Worth",
-            "Bills",
-            "Splitwise",
-          ].map((label) => (
-            <button
-              key={label}
-              className="btn btn-outline-success me-2"
-              type="button"
-            >
-              {label}
-            </button>
-          ))}
-        </div>
-      </nav>
-    </>
+      <div className="container-fluid justify-content-start">
+        {[
+          "Overview",
+          "Spending",
+          "Budget",
+          "Goals",
+          "Savings",
+          "Investments",
+          "Net Worth",
+          "Bills",
+          "Splitwise",
+        ].map((label) => (
+          <button
+            key={label}
+            className="btn btn-outline-success me-2"
+            type="button"
+          >
+            {label}
+          </button>
+        ))}
+      </div>
+    </nav>
   );
 }
 
