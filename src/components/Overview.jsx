e0c4030f185102a4dcfaddc3badda2d18bf4ed68
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-11-27 13:06:54 -0800

    i made a net worth card that goes in box-2 of the overview grid. it is basically a copy/paste with some changes to the logic of the function from the total cash card

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,11 +2,13 @@ import SnapshotCard from "./SnapshotCard";
 import AccountsSnapshot from "./AccountsSnapshot";
 import NetWorthGraph from "./NetWorthGraph";
 import OverviewCardCashTotal from "./OverviewCardCashTotal";
+import NetWorthCard from "./NetWorthCard";
 
 function Overview() {
   return (
     <div className="overview-grid">
       <OverviewCardCashTotal />
+      <NetWorthCard />
       <NetWorthGraph />
       <AccountsSnapshot />
     </div>
