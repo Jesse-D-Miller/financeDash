522b4f393f4971dbaf172a2d651207f872b8ea5d
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-11-27 13:13:29 -0800

    changed the accounts snapshot card to use mock data from accounts overview data

--- src/components/AccountsSnapshot.jsx
+++ src/components/AccountsSnapshot.jsx
@@ -1,70 +1,21 @@
+import data from "../data/account_balances_overview.json";
+
 function AccountsSnapshot() {
-  const accounts = [
-    {
-      id: "chk",
-      name: "Chequing",
-      type: "cash",
-      institution: "RBC",
-      balance: 2850,
-    },
-    {
-      id: "sav",
-      name: "High-Interest Savings",
-      type: "savings",
-      institution: "EQ",
-      balance: 12650,
-    },
-    {
-      id: "tfsa",
-      name: "TFSA Index",
-      type: "investment",
-      institution: "Wealthsimple",
-      balance: 21400,
-    },
-    {
-      id: "rrsp",
-      name: "RRSP",
-      type: "investment",
-      institution: "Wealthsimple",
-      balance: 34150,
-    },
-    {
-      id: "cc1",
-      name: "Visa (J)",
-      type: "credit",
-      institution: "RBC",
-      balance: -620,
-    },
-    {
-      id: "cc2",
-      name: "MC (W)",
-      type: "credit",
-      institution: "Tangerine",
-      balance: -380,
-    },
-    {
-      id: "mort",
-      name: "Mortgage",
-      type: "liability",
-      institution: "BMO",
-      balance: -412000,
-    },
-  ];
 
   return (
     <div className="accounts-snapshot-grid-item" style={{ gridArea: "box-5" }}>
       <h2>Accounts Snapshot</h2>
 
-      {accounts.map((account) => (
+      {data.accounts.map((account) => (
         <ul className="list">
           <li className="list-item institution">
-            {account.institution}
+            {account.bank}
           </li>
           <li className="list-item name">
-            {account.name}
+            {account.account_type}
           </li>
           <li className="list-item balance">
-            CA${account.balance}
+            CA$ {account.balance}
           </li>
         </ul>
       ))}
