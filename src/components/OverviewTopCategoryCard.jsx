52158cfffbf0bbb3dee951ef866151e3abe7b9b0
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-11-28 11:33:52 -0800

    i regenerated the credit card information to be better representative

--- src/components/OverviewTopCategoryCard.jsx
+++ src/components/OverviewTopCategoryCard.jsx
@@ -40,9 +40,32 @@ function OverviewTopCategoriesCard() {
   });
 
   //test where we should see only most recent months transactions
-  console.log(
-  latestMonthsTransactions.map((tx) => tx.date)
-);
+  console.log(latestMonthsTransactions.map((tx) => tx.date));
+
+  //take latest months transactions and sort them into an object by category. if key doesn't exist, create it and add amount. if key exists, add amount to total
+  const spendByCategory = {};
+  for (let transaction of latestMonthsTransactions) {
+    //this skips credit and payments so we only consider spending habits
+    if (transaction.amount < 0) continue;
+
+    const category = transaction.category;
+
+    spendByCategory[category] = (spendByCategory[category] || 0) + transaction.amount;
+  }
+
+  //now grab the top category and total amount spent on that category from the object
+  let topCategory = "";
+  let topCategoryAmount = 0;
+
+  //this object only holds 1 months spending so it should iterate quickly
+  for (let category in spendByCategory) {
+    const amount = spendByCategory[category];
+
+    if (amount > topCategoryAmount) {
+      topCategoryAmount = amount; //category to display
+      topCategory = category; //spending to display
+    }
+  }
 
   return (
     <div className="card-account-snapshot" style={{ gridArea: "box-3" }}>
@@ -59,8 +82,8 @@ function OverviewTopCategoriesCard() {
         <h7>Top Category</h7>
       </div>
       <div>
-        <p>{}</p>
-        <h7>CA$ {} ototal</h7>
+        <p>{topCategory}</p>
+        <h7>CA ${topCategoryAmount} total</h7>
       </div>
     </div>
   );
